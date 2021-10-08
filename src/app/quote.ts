023beb0393c1c29f04e96430abd2d4b6af7e6ee6
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-08 14:21:21 +0200

    define quote class properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,3 @@
 export class Quote {
+    constructor(public author:string,public user:string,public quote:string,public describe:string,public upvotes:number,public downvotes:number,public addDate:Date){}
 }
