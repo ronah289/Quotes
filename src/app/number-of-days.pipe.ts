cbf57d7f805fcbad5ffb555aee92e0fe68f8989f
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-10 09:39:06 +0200

    style form and quotes display

--- src/app/number-of-days.pipe.ts
+++ src/app/number-of-days.pipe.ts
@@ -6,18 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class NumberOfDaysPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today: Date = new Date(); //get current date and time
-    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(todayWithNoTime-value) //returns value in miliseconds
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    var dateDifferenceSeconds = dateDifference * 0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds / secondsInDay;
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(today.getDate(), today.getMonth(), today.getFullYear())
+    var dateDifference = Math.abs(todayWithNoTime - value)
+    const secondsInADay = 86400;
 
-    if (dateCounter >= 1 && value < todayWithNoTime) {
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter = dateDifferenceSeconds / secondsInADay;
+
+    if (dateCounter >= 1 && todayWithNoTime < value) {
       return dateCounter;
     } else {
       return 0;
     }
+
   }
 
 }
