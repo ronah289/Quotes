fbca30c8d1366775871032c75d779b257180d28e
ronah289 <ronald.kibet@student.moringaschool.com>
2021-10-11 14:33:10 +0200

    format display,add footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  copyright:string = '2021';
+  webb:string = 'quotes-base';
 
   constructor() { }
 
